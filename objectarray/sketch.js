a403d5980d8aa23b0fab39268c22c3d37af48003
HansonDestroy <161520032+HansonDestroy@users.noreply.github.com>
2024-03-28 12:18:49 -0600

    fdf

--- objectarray/sketch.js
+++ objectarray/sketch.js
@@ -10,20 +10,32 @@ let modes = ["normal", "practice", "single attack", "endless"];
 let mode = 0;
 let level = 0;
 let bones = [];
+let scaleOfPlayer;
 
+// level 1 platform
+let platform1={x: 0.5,y: 0.5,l: 0.275,w: 0.025};
+let platform2={x: 0.375,y: 0.625,l: 0.025,w: 0.275};
+let platform3={x: 0.5,y: 0.75,l: 0.275,w: 0.025};
+let platform4={x: 0.625,y: 0.625,l: 0.025,w: 0.275};
+// let platform1={x: 0.5,y: 0,l: 1,w: 0.025};
+// let platform2={x: 0,y: 0.5,l: 0.025,w: 1};
+// let platform3={x: 0.5,y: 1,l: 1,w: 0.025};
+// let platform4={x: 1,y: 0.5,l: 0.025,w: 1};
+let level1Platform=[platform1,platform2,platform3,platform4];
+
+let heart;
 let player = {
-  x: 0,
-  y: 0,
-  dx: 1,
-  dy: 1,
+  x: 3,
+  y: 3,
 };
 
 function preload() {
-  player = loadImage("heart.png");
+  heart = loadImage("heart.png");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  scaleOfPlayer = 0.00005 * width;
 }
 
 function draw() {
@@ -32,7 +44,10 @@ function draw() {
     drawStartScreen();
   }
   else{
+    innit();
     displayBones();
+    displayPlatorm();
+    displayPlayer();
   }
 }
 
@@ -61,22 +76,22 @@ function drawStartScreen(){
   let heartX = 0.45;
   if (mode === 0){
     imageMode(CENTER);
-    image(player, heartX * width , (titleHeight + modeHeightDifference * 2) * height, player.width * 0.08, player.height * 0.08);
+    image(heart, heartX * width , (titleHeight + modeHeightDifference * 2) * height, heart.width * scaleOfPlayer, heart.height * scaleOfPlayer);
   }
 
   if (mode === 1){
     imageMode(CENTER);
-    image(player, heartX * width , (titleHeight + modeHeightDifference * 3) * height, player.width * 0.08, player.height * 0.08);
+    image(heart, heartX * width , (titleHeight + modeHeightDifference * 3) * height, heart.width * scaleOfPlayer, heart.height * scaleOfPlayer);
   }
   
   if (mode === 2){
     imageMode(CENTER);
-    image(player, heartX * width , (titleHeight + modeHeightDifference * 4) * height, player.width * 0.08, player.height * 0.08);
+    image(heart, heartX * width , (titleHeight + modeHeightDifference * 4) * height, heart.width * scaleOfPlayer, heart.height * scaleOfPlayer);
   }
   
   if (mode === 3){
     imageMode(CENTER);
-    image(player, heartX * width , (titleHeight + modeHeightDifference * 5) * height, player.width * 0.08, player.height * 0.08);
+    image(heart, heartX * width , (titleHeight + modeHeightDifference * 5) * height, heart.width * scaleOfPlayer, heart.height * scaleOfPlayer);
   }
   
 }
@@ -91,20 +106,44 @@ function keyTyped(){
   }
   if (key === " " && state === "starting screen"){
     state = modes[mode];
+    level++;
   } 
   mode = mode % modes.length;
 }
 
-function displayBones(){
-  if (level === 1){
+function innit(){
+  if(level === 1){
+    imageMode(BOTTOM)
+    player.x = level1Platform[2].x * height;
+    player.y = level1Platform[2].y * height - level1Platform[0].w * height - heart.height * scaleOfPlayer / 2;
+    fill("white");
+    line(player.x, 0, player.x, height);
+    line(0, player.y, width, player.y);
   }
 }
 
-let platform1={x: 0.5,y: 0.5,l: 0.25,w: 0.05};
-let platform2={x: 0.375,y: 0.625,l: 0.05,w: 0.25};
-let platform3={x: 0.5,y: 0.75,l: 0.25,w: 0.05};
-let platform4={x: 0.625,y: 0.625,l: 0.05,w: 0.25};
-let level1Platform=[platform1,platform2,platform3,platform4];
+function displayBones(){
+  if (level === 1){
+    let i;
+  }
+}
+
+function displayPlatorm(){
+  if (level === 1){
+    for (let platform of level1Platform){
+      rectMode(CENTER);
+      fill("white");
+      noStroke();
+      rect(platform.x * height, platform.y * height, platform.l * height, platform.w * height);
+      rectMode(LEFT);
+    }
+  }
+}
+
+function displayPlayer(){
+  image(heart, player.x, player.y, heart.width * scaleOfPlayer, heart.height * scaleOfPlayer);
+}
+
 
 // let y;
 // let dx = 2;
