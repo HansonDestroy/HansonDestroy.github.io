982669fc737dbad0a25f26b692d92a8daa66cc10
HansonDestroy <161520032+HansonDestroy@users.noreply.github.com>
2024-05-10 10:50:47 -0600

    sdfsdfs

--- p5-play/sketch.js
+++ p5-play/sketch.js
@@ -5,18 +5,33 @@ function setup() {
 
 	monster = new Sprite();
 	monster.img = 'heart.png';
-  monster.scale = 0.05
+	monster.scale = 0.05
 	monster.diameter = 70;
 	monster.x = 50
-  monster.y = 50
-	hi = new Sprite(100,50,60,60,"k")
+	monster.y = 50
+	player = new Sprite();
+	player.img = 'heart.png';
+	player.scale = 0.05
+	player.diameter = 70;
+	player.x = 50
+	player.y = 50
+	player.collider = "n"
+	hi = new Sprite(100,200,60,60,"k")
 }
 
 function draw() {
 	clear();
-  monster.moveTowards(mouse)
+	if (keyIsDown(83)){
+		monster.x = monster.x + 2;
+	}
+	if (keyIsDown(86)){
+		monster.y = monster.y + 2;
+	}
+	//monster.moveTowards(mouse)
+	player.x = monster.x
+	player.y = monster.y
 	if (kb.presses(2)) {
-		monster.scale = 1;
+		monster.scale = monster.scale / 2.2;
 	}
 	monster.debug = mouse.pressing();
 }
